e0ae7dd60664aea122d6e875a9d3435d6b92474b
Minh Chau <emxi.dev@gmail.com>
2024-01-07 11:03:22 +0700

    f

--- Akka-Coffee/MainWindow.xaml.cs
+++ Akka-Coffee/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Akka.Actor;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,36 @@ namespace Akka_Coffee
     /// </summary>
     public partial class MainWindow : Window
     {
+        IActorRef managerActor;
+        IActorRef orderActor;
+        IActorRef billingActor;
+        IActorRef productProcessingActor;
+
+        List<Product> products;
+
         public MainWindow()
         {
             InitializeComponent();
+            var system = ActorSystem.Create("CafeSystem");
+            managerActor = system.ActorOf(Props.Create(() => new ManagerActor()), "ManagerActor");
+
+            orderActor = system.ActorOf(Props.Create(() => new OrderActor()), "OrderActor");
+            billingActor = system.ActorOf(Props.Create(() => new BillingActor()), "BillingActor");
+            productProcessingActor = system.ActorOf(Props.Create(() => new ProductProcessingActor()), "ProductProcessingActor");
+
+            products = new List<Product>()
+            {
+                new Product("Black coffee", 30000),
+                new Product("White coffee", 35000),
+            };
+
+            //managerActor.Tell(new DisplayInprogressProductsMessage());
+
+        }
+
+        private void ShowInprogressProducts_Click(object sender, RoutedEventArgs e)
+        {
+            managerActor.Tell(new DisplayInprogressProductsMessage());
         }
     }
 }
